08721bae0c90cd5d86a80294c2b90725a9323496
BillyDaPoop <65975856+BillyDaPoop@users.noreply.github.com>
2020-08-14 08:29:53 -0700

    Added an optimization guide for fabric
    Needs work

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   someSidebar: {
     'Overview': ['overview'],
     'Basics': ['basic-controls', 'scheduling-actions', 'how-to-use-sftp', 'editing-account-details', 'pointing-a-domain'],
+    'Server Optimization': ['fabric-server-optimization'],
     'Extras': ['mobile-app', 'reverse-proxy',],
   },
 };
