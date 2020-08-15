dfb5938a5239d76a0e56a93fbe9b8d737bb46690
LoJoSho <37521985+LoJoSho@users.noreply.github.com>
2020-08-15 12:45:55 -0500

    Added server-id

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Overview': ['overview'],
-    'Basics': ['basic-controls', 'scheduling-actions', 'how-to-use-sftp', 'editing-account-details', 'pointing-a-domain'],
+    'Basics': ['basic-controls', 'scheduling-actions', 'how-to-use-sftp', 'editing-account-details', 'pointing-a-domain', 'server-id'],
     'Server Optimization': ['fabric-server-optimization'],
     'Plugins': ['chunky'],
     'Extras': ['mobile-app', 'reverse-proxy',],
