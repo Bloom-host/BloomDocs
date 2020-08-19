a0af2f56c3b254031d9aeeeaae5a2b9161757eb6
Amgelo563 <61554601+Amgelo563@users.noreply.github.com>
2020-08-19 12:22:18 -0500

    Fixed sidebar

--- sidebars.js
+++ sidebars.js
@@ -3,21 +3,23 @@ module.exports = {
     'Overview': ['overview'],
     'Basics': [
       {
-        'Panel Controls': ['basic-controls', 'scheduling-actions', 'how-to-use-sftp', 'sub-users', 'databases', 'server-id', 'file-manager-controls','2fa']
-      }, 'editing-account-details', 'plugins', 'pointing-a-domain', 'whichsoftwaretouse', 'setting-up-forge','server-resource-packs'],
+        'Panel Controls': ['basic-controls', 'scheduling-actions', 'how-to-use-sftp', 'sub-users', 'databases', 'server-id', 'file-manager-controls', '2fa']
+      }, 'editing-account-details', 'plugins', 'pointing-a-domain', 'whichsoftwaretouse', 'setting-up-forge', 'server-resource-packs'],
     'Server Optimization': ['paper-server-optimization', 'fabric-server-optimization'],
     'Plugins': ['plugins/chunky', 'plugins/luckperms', 'plugins/prism', 'plugins/geysermc'],
-    'Extras': ['mobile-app', 'reverse-proxy','adding-an-icon'],
+    'Extras': ['mobile-app', 'reverse-proxy', 'adding-an-icon'],
     'Other Languages': [
       {
         'Spanish': [
           {
-            'Controles del Panel': [
+            'Lo Básico': [
               {
-                'languages/spanish/controles-basicos', 'languages/spanish/horarios', 'languages/spanish/sftp', 'languages/spanish/sub-usuarios', 'languages/spanish/databases', 'languages/spanish/id-servidor', 'languages/spanish/gestion-archivos', 'languages/spanish/2fa']
-              }
-          }
-          'Otros':['languages/spanish/configurar-un-dominio', 'languages/spanish/configurar-whitelist', 'languages/spanish/instalar-plugins']
-      }, ],
+                'Controles del Panel': ['languages/spanish/controles-basicos', 'languages/spanish/horarios', 'languages/spanish/sftp', 'languages/spanish/sub-usuarios', 'languages/spanish/databases', 'languages/spanish/id-servidor', 'languages/spanish/gestion-archivos', 'languages/spanish/2fa']
+              },],
+            'Otros': ['languages/spanish/configurar-un-dominio', 'languages/spanish/configurar-whitelist', 'languages/spanish/instalar-plugins'],
+          },
+        ],
+      },
+    ],
   },
-};
+}
