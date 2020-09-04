72f054d44166a3405fdb4491a8d514b71af3b241
Amgelo563 <luisangeloarce314@gmail.com>
2020-09-04 01:55:46 -0500

    Added MOTD article - Spanish

---
+++ dist/sidebars.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  someSidebar: {
+    'Overview': ['overview'],
+    'Basics': [{
+      'Panel Controls': ['basic-controls', 'scheduling-actions', 'how-to-use-sftp', 'sub-users', 'databases', 'server-id', 'file-manager-controls', '2fa', 'motd']
+    }, 'editing-account-details', 'plugins', 'pointing-a-domain', 'whichsoftwaretouse', 'setting-up-forge', 'server-resource-packs', 'datapacks'],
+    'Server Optimization': ['paper-server-optimization', 'fabric-server-optimization'],
+    'Plugins': ['plugins/chunky', 'plugins/luckperms', 'plugins/prism', 'plugins/geysermc', 'plugins/bungeeguard', 'plugins/viaversion', 'plugins/dynmap', 'plugins/votifier'],
+    'Extras': ['mobile-app', 'reverse-proxy', 'adding-an-icon'],
+    'Other Languages': [{
+      'Español': [{
+        'Lo Básico': [{
+          'Controles del Panel': ['languages/spanish/basico/panel/controles-basicos', 'languages/spanish/basico/panel/horarios', 'languages/spanish/basico/panel/sftp', 'languages/spanish/basico/panel/sub-usuarios', 'languages/spanish/basico/panel/databases', 'languages/spanish/basico/panel/id-servidor', 'languages/spanish/basico/panel/gestion-archivos', 'languages/spanish/basico/panel/2fa']
+        }, 'languages/spanish/basico/software', 'languages/spanish/basico/motd', 'languages/spanish/basico/instalar-plugins', 'languages/spanish/basico/configurar-un-dominio', 'languages/spanish/basicoconfigurar-whitelist']
+      }]
+    }]
+  }
+};
