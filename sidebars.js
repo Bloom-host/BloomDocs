0ce990ae5c81547072391386cf297c171826e560
Amgelo563 <luisangeloarce314@gmail.com>
2020-09-04 01:43:02 -0500

    Making sidebars match so fork can get updated

--- sidebars.js
+++ sidebars.js
@@ -7,18 +7,10 @@ module.exports = {
       }, 'editing-account-details', 'plugins', 'pointing-a-domain', 'whichsoftwaretouse', 'setting-up-forge','server-resource-packs', 'datapacks'],
     'Server Optimization': ['paper-server-optimization', 'fabric-server-optimization'],
     'Plugins': ['plugins/chunky', 'plugins/luckperms', 'plugins/prism', 'plugins/geysermc', 'plugins/bungeeguard', 'plugins/viaversion', 'plugins/dynmap', 'plugins/votifier'],
-    'Extras': ['mobile-app', 'reverse-proxy', 'adding-an-icon'],
+    'Extras': ['mobile-app', 'reverse-proxy','adding-an-icon'],
     'Other Languages': [
       {
-        'Español': [
-          {
-            'Lo Básico': [
-              {
-                'Controles del Panel': ['languages/spanish/basico/panel/controles-basicos', 'languages/spanish/basico/panel/horarios', 'languages/spanish/basico/panel/sftp', 'languages/spanish/basico/panel/sub-usuarios', 'languages/spanish/basico/panel/databases', 'languages/spanish/basico/panel/id-servidor', 'languages/spanish/basico/panel/gestion-archivos', 'languages/spanish/basico/panel/2fa']
-              }, 'languages/spanish/software', 'languages/spanish/basico/instalar-plugins', 'languages/spanish/basico/configurar-un-dominio', 'languages/spanish/configurar-whitelist'],
-          },
-        ],
-      },
-    ],
+        'Spanish': ['languages/spanish/configurar-un-dominio', 'languages/spanish/configurar-whitelist', 'languages/spanish/instalar-plugins']
+      }, ],
   },
-}
+};
