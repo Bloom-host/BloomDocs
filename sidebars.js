acaa5274db4b1e0a7e1e27de5a3cd7fa1e790d9c
EuanM199 <67420304+EuanM199@users.noreply.github.com>
2020-09-08 21:27:46 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -21,12 +21,11 @@ module.exports = {
             'Extras': ['languages/spanish/extras/reverse-proxy', 'languages/spanish/extras/contributing']
           },
         ],
-          {
+          
             'Italiano': [
               {
                 'Controles del Panel': ['languages/italiano/controlli/controlli-di-base']
-              }, 
-          },
+              },
         ],
       },
     ],
