3b667830c6c31b246f3efd04e89decd2d2c63cf9
alau740 <77434904+alau740@users.noreply.github.com>
2021-01-27 23:11:18 +0000

    should hopefully have fixed

--- sidebars.js
+++ sidebars.js
@@ -37,7 +37,7 @@ module.exports = {
       {
         'Plugins': [
           'plugins/advancedanticheat',
-          'plugins/AdvancedBan',
+          'plugins/advancedban',
           'plugins/bungeeguard', 
           'plugins/chunky', 
           'plugins/citizens2',
@@ -47,7 +47,7 @@ module.exports = {
           'plugins/discordsrv', 
           'plugins/dynmap', 
           'plugins/geysermc', 
-          'plugins/GriefPrevention',
+          'plugins/griefprevention',
           'plugins/Litebans', 
           'plugins/luckperms', 
           'plugins/Milk', 
