c8a70f1b71de3796d93813aec48d40148dc0e99f
Evidentsinger14 <46200123+Evidentsinger14@users.noreply.github.com>
2022-03-12 22:19:57 -0500

    add ban announcer to sidebar

--- sidebars.js
+++ sidebars.js
@@ -154,6 +154,7 @@ module.exports = {
                     items: [ // Note: Chunky, LiteBans, LuckPerms and PLAN pages were moved to multiplatform category
                         'plugins_and_modifications/plugins/advancedanticheat', // anticheat
 			'plugins_and_modifications/plugins/ajparkour' // ajParkour
+			'plugins_and_modifications/plugins/banannouncer' // announce bans in discord
                         'plugins_and_modifications/plugins/bungeeguard', // BungeeCord security (prevents UUID spoof exploit)
                         'plugins_and_modifications/plugins/citizens2',
                         'plugins_and_modifications/plugins/conditionalcommands',
