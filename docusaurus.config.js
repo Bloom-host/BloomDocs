bab4ccb02038bf6871b97e5da71d2cc3eae8dc40
NotGeri <46295973+NotGeri@users.noreply.github.com>
2023-08-19 14:50:51 +0200

    fix: use correct sidebar colour
    closes #276

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,97 +1,96 @@
 module.exports = {
-  title: "Docs",
-  tagline: "Docs for Bloom",
-  url: "https://docs.bloom.host",
-  baseUrl: "/",
-  onBrokenLinks: "warn",
-  favicon: "imgs/favicon.ico",
-  organizationName: "bloom-host",
-  projectName: "bloomdocs",
+    title: 'Docs',
+    tagline: 'Docs for Bloom',
+    "url": "https://docs.bloom.host",
+    baseUrl: '/',
+    onBrokenLinks: 'warn',
+    favicon: 'imgs/favicon.ico',
+    organizationName: 'bloom-host',
+    projectName: 'bloomdocs',
 
-  plugins: [
-    require.resolve('docusaurus-plugin-image-zoom')
-  ],
-
-  themeConfig: {
-    image: 'imgs/meta.png',
-    colorMode: {
-      defaultMode: "dark",
-      disableSwitch: false,
-    },    
-    metadata: [
-      { name: 'og:site_name', content: 'Bloom.host' },
-      { name: 'theme-color', content: '#ff6300' },
+    plugins: [
+        require.resolve('docusaurus-plugin-image-zoom')
     ],
 
-    algolia: {
-      apiKey: "6e10e6fc7bbd9b5093c45f689e51d2df",
-      appId: 'INL0MGKMMA',
-      indexName: "bloom",
-      contextualSearch: true,
-      searchParameters: {},
+    themeConfig: {
+        image: 'imgs/meta.png',
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: false,
+        },
+
+        metadata: [
+            {name: 'og:site_name', content: 'Bloom.host'},
+            {name: 'theme-color', content: '#ff6300'},
+        ],
+
+        algolia: {
+            apiKey: "6e10e6fc7bbd9b5093c45f689e51d2df",
+            appId: 'INL0MGKMMA',
+            indexName: "bloom",
+            contextualSearch: true,
+            searchParameters: {},
+        },
+
+        zoom: {
+            selector: '.markdown :not(em) > img',
+            background: {
+                light: 'rgb(255, 255, 255)',
+                dark: 'rgb(33, 33, 50)'
+            },
+            config: {}
+        },
+
+        navbar: {
+            title: 'Bloom Docs',
+            logo: {
+                alt: 'Bloom Logo',
+                src: 'imgs/favicon.ico'
+            },
+            items: [
+                {
+                    type: 'localeDropdown',
+                    position: 'right',
+                },
+                {
+                    href: 'https://bloom.host/',
+                    label: 'Website',
+                },
+                {
+                    href: 'https://discord.gg/bloom',
+                    label: 'Support Discord',
+                },
+                {
+                    href: 'https://billing.bloom.host/supporttickets.php',
+                    label: 'Billing Support',
+                },
+            ],
+        },
+
+        footer: {
+            style: 'dark',
+            copyright: `Copyright © ${new Date().getFullYear()} Super Duper Broccoli, Inc. Built with Docusaurus.`,
+        },
     },
 
-    zoom: {
-      selector: '.markdown :not(em) > img',
-      background: {
-        light: 'rgb(255, 255, 255)',
-        dark: 'rgb(33, 33, 50)'
-      },
-      config: {
-        // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
-      }
-    },
-
-    navbar: {
-      title: "Bloom Docs",
-      logo: {
-        alt: "Bloom Logo",
-        src: "imgs/favicon.ico"
-      },
-      items: [
-        {
-          type: 'localeDropdown',
-          position: 'right',
-        },
-        {
-          href: 'https://bloom.host/',
-          label: 'Website',          
-        },
-        {
-          href: 'https://discord.gg/bloom',
-          label: 'Support Discord',
-        },
-        {
-          href: 'https://billing.bloom.host/supporttickets.php',
-          label: 'Billing Support',
-        },
-      ],
-    },
-
-    footer: {
-      style: "dark",
-      copyright: `Copyright © ${new Date().getFullYear()} Super Duper Broccoli, Inc. Built with Docusaurus.`,
-    },
-  },
-
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    routeBasePath: '/',
+                    sidebarPath: require.resolve('./sidebars.js'),
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
     ],
-  ],
 
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-  },
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'es'],
+    },
 
 };
